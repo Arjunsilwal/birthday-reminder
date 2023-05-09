6cd2f30e5ee2a6e721a9ddb783acee567a984bc9
Arjun <Silwal>
2023-05-09 12:46:10 +1000

    date is displayed using map method

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import List from "./List";
 import data from "./data";
 
 function App() {
-	const [birthday, setBirthday] = useState({ data });
+	const [birthday, setBirthday] = useState(data);
 	return (
 		<main>
 			<section className="container">
 				<h3>5 people have birthdays today</h3>
 				<List people={birthday} />
-				<button>Clear All</button>
+				<button onClick={() => console.log("you clicked me")}>Clear All</button>
 			</section>
 		</main>
 	);
